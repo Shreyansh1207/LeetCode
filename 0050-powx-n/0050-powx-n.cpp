3d4657445ba218e1ce479ccef79c4d09d9ad62a5
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2024-07-31 13:13:00 +0530

    Time: 0 ms (100%), Space: 8.4 MB (6.52%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,17 +1,7 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-       double ans;
-       long long N=n;
-         
-            if(n<0){
-                N=-N;
-                ans=1/pow(x,N);
-            }
-            else{
-                ans=pow(x,N);
-            }
-        
-       return ans;
+        double power = pow(x,n);
+        return power;
     }
 };
