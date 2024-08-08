d54d1b84f1587913835de9c58e5b248cdac4cd01
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2024-08-08 18:33:46 +0530

    Time: 16 ms (18.44%), Space: 22.5 MB (39.55%) - LeetHub

--- 3330-modify-the-matrix/3330-modify-the-matrix.cpp
+++ 3330-modify-the-matrix/3330-modify-the-matrix.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> modifiedMatrix(vector<vector<int>>& matrix) {
-        
+         int rows=matrix.size();
+         int colmn=matrix[0].size();
+        for(int i=0;i<rows;++i){
+            for(int j=0;j<colmn;++j){
+                if(matrix[i][j]==-1){
+                    int y=0;
+                    for(int k=0;k<rows;++k){
+                        if(matrix[k][j]>y){
+                            y=matrix[k][j];
+                        }
+                    }
+
+                
+                matrix[i][j]=y;
+            }
+                 
+            }
+        }   
+        return matrix;
     }
 };
