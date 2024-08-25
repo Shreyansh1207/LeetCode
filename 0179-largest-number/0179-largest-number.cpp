b0701f408637d0886f7c55a4f1fd3097a752770b
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2024-08-25 11:58:14 +0530

    Time: 3 ms (92.41%), Space: 17.3 MB (27.98%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> numStrs;
+        for(int num : nums) {
+            numStrs.push_back(to_string(num));
+        }
+
+        sort(numStrs.begin(), numStrs.end(), [](string &a, string &b) {
+            return a + b > b + a;
+        });
+
+        if(numStrs[0] == "0") return "0";
+
+        string largestNum = "";
+        for(string &numStr : numStrs) {
+            largestNum += numStr;
+        }
+
+        return largestNum;
+    }
+};
