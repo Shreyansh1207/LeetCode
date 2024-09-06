35d87d3acd4110220ce925fd61b35d6aa2bb7e12
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2024-09-06 11:53:00 +0530

    Time: 7 ms (47.57%), Space: 16.7 MB (5.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,19 +1,54 @@
+int firstOcc(vector<int>nums,int target){
+    int s=0;
+    int e=nums.size()-1;
+    int ans=-1;
+    while(s<=e){
+       int mid=s+(e-s)/2;
+       if(target==nums[mid]){
+        ans=mid;
+        e=mid-1;
+       }
+       else if(target<nums[mid]){
+        e=mid-1;
+       }
+       else{
+        s=mid+1;
+       }
+       mid=s+(e-s)/2;
+
+    }
+    return ans;
+
+}
+int lastOcc(vector<int>nums,int target){
+     int s=0;
+    int e=nums.size()-1;
+    int ans=-1;
+    while(s<=e){
+       int mid=s+(e-s)/2;
+       if(target==nums[mid]){
+        ans=mid;
+        s=mid+1;
+       }
+       else if(target<nums[mid]){
+        e=mid-1;
+       }
+       else{
+        s=mid+1;
+       }
+       mid=s+(e-s)/2;
+
+    }
+    return ans;
+
+}
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>res(2,-1);
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-            res[0]=i;
-            break;
-            }
-        }
-        for(int i=nums.size()-1;i>=0;i--){
-            if(nums[i]==target){
-            res[1]=i;
-            break;
-            }
-        }
-        return res;
+        vector<int>ans;
+        ans.push_back(firstOcc(nums,target));
+        ans.push_back(lastOcc(nums,target));
+        return ans;
+        
     }
 };
