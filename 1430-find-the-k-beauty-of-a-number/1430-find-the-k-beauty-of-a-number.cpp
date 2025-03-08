a9b154a1ae04cd3dc069a1d0ee4d8e458155483b
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2025-03-08 15:11:25 +0530

    Time: 0 ms (100%), Space: 8.2 MB (8.45%) - LeetHub

--- 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.cpp
+++ 1430-find-the-k-beauty-of-a-number/1430-find-the-k-beauty-of-a-number.cpp
@@ -3,15 +3,15 @@ public:
     int divisorSubstrings(int num, int k) {
         auto s=to_string(num);
         int ans=0;
-        for(int i=0;i<s.size();i++){
-            for(int j=i;j<s.size();j++){
-                if(j-i+1==k){
+       int i=0,j=0;
+       while(j<s.size()){
+           if(j-i+1==k){
                     auto cur=s.substr(i,j-i+1);
                     auto curnum=stoi(cur);
-                    if(curnum==0)continue;
-                    if(num%curnum==0)ans++;
-                }
-            }
-        }return ans;
+                    if(curnum!=0&&num%curnum==0)ans++;
+                    i++;
+           }
+           j++;
+       }return ans;
     }
 };
