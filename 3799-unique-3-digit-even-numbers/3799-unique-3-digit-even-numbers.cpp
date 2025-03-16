909bdb431dfa7671dbe13ed113b6078b4f5393ef
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2025-03-16 22:03:19 +0530

    Time: 7 ms (72.73%), Space: 30.9 MB (36.36%) - LeetHub

--- 3799-unique-3-digit-even-numbers/3799-unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/3799-unique-3-digit-even-numbers.cpp
@@ -1,21 +1,8 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        // int count =0;
-        // for(int i=0;i<digits.size();i++){
-        //     int j=i+1;
-        //     int k=digits.size()-1;
-        //     while(j<k){
-        //         int n=digits[i]*100+digits[j]*10+digits[k];
-        //         if(digits[i]!=0 && (n)%2==0){
-        //             count++;
-        //             k--;
-        //             j++;
-        //         }
-        //     }
-        // }
-        // return count;
-        unordered_set<int> uniqueNumbers;
+        
+        unordered_set<int> num;
         int count=0;
         for(int i=0;i<digits.size();i++){
             if(digits[i]==0)continue;
@@ -25,12 +12,13 @@ public:
                     if(k==i || k==j)continue;
                     int n=digits[i]*100+digits[j]*10+digits[k];
                     if(n%2==0){
-                        uniqueNumbers.insert(n);
+                        num.insert(n);
                     }
 
                 }
             }
         }
-        return uniqueNumbers.size();
+        return num.size();
+        
     }
 };
