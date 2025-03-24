e02819b35e8b39f983b0b705c60379c527712eeb
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2025-03-25 00:43:31 +0530

    Time: 1339 ms (7.65%), Space: 389.7 MB (11.37%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int solve(string &s,int i,int j,vector<vector<int>>&dp){
+        if(i>=j){
+            return 1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s[i]==s[j]){
+            return solve(s,i+1,j-1,dp);
+        }
+        return dp[i][j]=0;
+    }
+    int countSubstrings(string s) {
+        int count=0;
+        vector<vector<int>>dp(1001,vector<int>(1001,-1));
+        for (int i = 0; i < s.size(); i++) {
+            for (int j = i; j < s.size(); j++) {
+                if (solve(s, i, j,dp) == true) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
