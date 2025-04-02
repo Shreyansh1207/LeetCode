cb494dd4e23a20176aa742e030f681d2aab48d4f
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2025-04-02 13:39:27 +0530

    Time: 5 ms (62.8%), Space: 127.7 MB (12.04%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.cpp
@@ -1,42 +1,24 @@
 class Solution {
 public:
-    // int solve(vector<vector<int>>questions,int i,vector<int>&dp){
-    //     int row=questions.size();
-    //     int col=questions[0].size();
-    //     if(i>=row){
-    //         return 0;
-    //     }
-    //     if(dp[i]!=-1){
-    //         return dp[i];
-    //     }
-    //     int ch1,ch2;
-    //     ch1=questions[i][0]+solve(questions,i+questions[i][1] + 1,dp);
-    //     ch2=solve(questions,i+1,dp);
-
-    //     return dp[i]=max(ch1,ch2);
-    // }
-    
-
-    long long mostPoints(vector<vector<int>>& questions) {
-        int n = questions.size();
-        vector<long long> dp(n + 1, 0); // DP array to store max points
-
-        for (int i = n - 1; i >= 0; i--) {
-            int points = questions[i][0];      // Current question's points
-            int jump = questions[i][1];        // Brainpower needed
-            int nextIndex = i + jump + 1;      // Next possible index
-            
-            // Option 1: Take this question
-            long long take = points + (nextIndex < n ? dp[nextIndex] : 0);
-            
-            // Option 2: Skip this question
-            long long skip = dp[i + 1];
-
-            // Store the best choice in DP
-            dp[i] = max(take, skip);
+    long long solve(vector<vector<int>>& questions, int i, vector<long long>& dp){
+        int row=questions.size();
+        int col=questions[0].size();
+        if(i>=row){
+            return 0;
         }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        long long ch1,ch2;
+        int j=questions[i][1]+1;
+        ch1=questions[i][0]+solve(questions,i+j,dp);
+        ch2=solve(questions,i+1,dp);
 
-        return dp[0]; // The max points possible from index 0
+        return dp[i]=max(ch1,ch2);
     }
+    long long mostPoints(vector<vector<int>>& questions) {
 
+        vector<long long>dp(questions.size()+1,-1);
+        return solve(questions,0,dp);
+    }
 };
