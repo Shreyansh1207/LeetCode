578fba3c79ba8180d8dc92dae00a7b77b7b47b8d
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2025-08-01 16:48:59 +0530

    Time: 60 ms (59.15%), Space: 22.4 MB (56.99%) - LeetHub

--- 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
@@ -3,12 +3,12 @@ public:
     int triangularSum(vector<int>& nums) {
         int a=nums.size();
         while(a--){
-            vector<int>v;
+            //vector<int>v;
             for(int i=0;i<a;i++){
-                int b=(nums[i]+nums[i+1])%10;
-                v.push_back(b);
+                nums[i]=(nums[i]+nums[i+1])%10;
+               // v.push_back(b);
             }
-            nums=v;
+            //nums=v;
         }
         return nums[0];
     }
