a467919033b174f920b03015135c8b4b8f85188b
Shreyansh1207 <131813012+Shreyansh1207@users.noreply.github.com>
2026-04-25 17:11:26 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int i=0;
+        int j=0;
+        int k=nums.size()-1;
+        while(j<=k){
+            if(nums[j]==2){
+                swap(nums[j],nums[k]);
+                k--;
+            }
+            else if(nums[j]==0){
+                swap(nums[j],nums[i]);
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+      
     }
 };
